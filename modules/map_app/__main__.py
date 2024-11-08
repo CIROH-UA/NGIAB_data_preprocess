11d2f89d9794461bf1176439f1f42a3be2f7a02d
Josh Cunningham <josh.cu@gmail.com>
2024-11-07 19:08:12 -0600

    ported essential map functions to maplibre

--- modules/map_app/__main__.py
+++ modules/map_app/__main__.py
@@ -14,27 +14,6 @@ from data_sources.source_validation import download_file, decompress_gzip_tar, v
 
 validate_all()
 
-if not file_paths.tiles_tms.is_dir():
-    print("Downloading catchment boundary map")
-    download_file(
-        "https://www.hydroshare.org/resource/f37d4b10e28a4694825c30fc38e2a7a0/data/contents/tms8-11.tar.gz",
-        "temp/tms.tar.gz",
-    )
-    print("Unzipping catchment boundary map")
-    decompress_gzip_tar("temp/tms.tar.gz", file_paths.tiles_tms)
-    os.remove("temp/tms.tar.gz")
-
-if not file_paths.tiles_vpu.is_dir():
-    print("Downloading vpu boundary map")
-    download_file(
-        "https://www.hydroshare.org/resource/f37d4b10e28a4694825c30fc38e2a7a0/data/contents/vpu.tar.gz",
-        "temp/vpu.tar.gz",
-    )
-    print("Unzipping vpu boundary map")
-    decompress_gzip_tar("temp/vpu.tar.gz", file_paths.tiles_vpu.parent)
-    os.remove("temp/vpu.tar.gz")
-
-
 with open("app.log", "w") as f:
     f.write("")
     f.write("Starting Application!\n")
--- modules/map_app/static/js/main.js
+++ modules/map_app/static/js/main.js
@@ -1,11 +1,3 @@
-var cat_id_dict = {};
-var selected_cat_layer = null;
-var upstream_maps = {};
-var flowline_layers = {};
-
-var registered_layers = {}
-
-
 var colorDict = {
     selectedCatOutline: getComputedStyle(document.documentElement).getPropertyValue('--selected-cat-outline'),
     selectedCatFill: getComputedStyle(document.documentElement).getPropertyValue('--selected-cat-fill'),
@@ -17,326 +9,6 @@ var colorDict = {
     nexusFill: getComputedStyle(document.documentElement).getPropertyValue('--nexus-fill'),
     clearFill: getComputedStyle(document.documentElement).getPropertyValue('--clear-fill')
 };
-
-
-async function update_selected() {
-    console.log('updating selected');
-    if (!(Object.keys(cat_id_dict).length === 0)) {
-        return fetch('/get_geojson_from_catids', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(cat_id_dict),
-        })
-            .then(response => response.json())
-            .then(data => {
-                // if the cat_id is already in the dict, remove the key
-                // remove the old layer
-                // layer = map.getLayer('selected')
-                map.getSource('selected').setData(data);
-                console.log(data);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    } else {
-        if (selected_cat_layer) {
-            map.removeLayer('selected');
-        }
-        return Promise.resolve();
-    }
-}
-
-async function populate_upstream() {
-    console.log('populating upstream selected');
-    // drop any key that is not in the cat_id_dict
-    for (const [key, value] of Object.entries(upstream_maps)) {
-        if (!(key in cat_id_dict)) {
-            map.removeLayer(value);
-            delete upstream_maps[key];
-        }
-    }
-    // add any key that is in the cat_id_dict but not in the upstream_maps
-    for (const [key, value] of Object.entries(cat_id_dict)) {
-        if (!(key in upstream_maps)) {
-            upstream_maps[key] = null;
-        }
-    }
-    if (Object.keys(upstream_maps).length === 0) {
-        return Promise.resolve();
-    }
-
-    const fetchPromises = Object.entries(upstream_maps).map(([key, value]) => {
-        if (value === null) {
-            return fetch('/get_upstream_geojson_from_catids', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(key),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    // if the cat_id is already in the dict, remove the key
-                    // remove the old layer
-                    if (upstream_maps[key]) {
-                        map.removeLayer(upstream_maps[key]);
-                    }
-                    console.log(data);
-                    // add the new layer if the downstream cat's still selected
-                    if (key in cat_id_dict) {
-                        layer_group = map.addLayer({
-                            'id': 'upstream',
-                            'type': 'fill',
-                            'source': { 'type': 'geojson', 'data': data },
-                            'layout': {},
-                            'paint': {
-                                'fill-color': '#088',
-                                'fill-opacity': 0.8
-                            }
-                        });
-                        upstream_maps[key] = layer_group;
-                        layer_group.eachLayer(function (layer) {
-                            if (layer._path) {
-                                layer._path.classList.add('upstream-cat-layer');
-                            }
-                        });
-                    }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        }
-    });
-    return fetchPromises;
-
-}
-
-async function populate_flowlines() {
-    console.log('populating flowlines');
-    // drop any key that is not in the cat_id_dict
-    for (const [key, value] of Object.entries(flowline_layers)) {
-        if (!(key in cat_id_dict)) {
-            for (i of flowline_layers[key]) {
-                map.removeLayer(i);
-                delete flowline_layers[key];
-            }
-        }
-    }
-    // add any key that is in the cat_id_dict but not in the flowline_layers
-    for (const [key, value] of Object.entries(cat_id_dict)) {
-        if (!(key in flowline_layers)) {
-            flowline_layers[key] = null;
-        }
-    }
-    if (Object.keys(flowline_layers).length === 0) {
-        return Promise.resolve();
-    }
-
-    const fetchPromises = Object.entries(flowline_layers).map(([key, value]) => {
-        if (value === null) {
-            return fetch('/get_flowlines_from_catids', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(key),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    // if the cat_id is already in the dict, remove the key
-                    // remove the old layer
-                    if (flowline_layers[key]) {
-                        for (i of flowline_layers[key]) {
-                            map.removeLayer(i);
-                        }
-                    }
-                    // loud!
-                    // console.log(data);
-                    to_cat = JSON.parse(data['to_cat']);
-                    to_nexus = JSON.parse(data['to_nexus']);
-                    nexus = JSON.parse(data['nexus']);
-                    // add the new layer if the downstream cat's still selected
-                    if (key in cat_id_dict) {
-                        to_cat_layer = maplibregl.geoJSON(to_cat).addTo(map);
-                        to_nexus_layer = maplibregl.geoJSON(to_nexus).addTo(map);
-                        nexus_layer = maplibregl.geoJSON(nexus).addTo(map);
-                        // hack to add css classes to the flowline layers
-                        // using eachLayer as it waits for layer to be done updating
-                        // directly accessing the _layers keys may not always work
-                        to_cat_layer.eachLayer(function (layer) {
-                            if (layer._path) {
-                                layer._path.classList.add('flowline-to-cat-layer');
-                            }
-                        });
-                        to_nexus_layer.eachLayer(function (layer) {
-                            if (layer._path) {
-                                layer._path.classList.add('flowline-to-nexus-layer');
-                            }
-                        });
-                    }
-                    flowline_layers[key] = [to_cat_layer, to_nexus_layer, nexus_layer];
-                })
-
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-        }
-    });
-    return fetchPromises;
-
-}
-
-async function synchronizeUpdates() {
-    console.log('Starting updates');
-
-    // wait for all promises
-    const selectedPromise = await update_selected();
-    // const upstreamPromises = await populate_upstream();
-    // const flowlinePromises = await populate_flowlines();
-    await Promise.any([selectedPromise, ...upstreamPromises, ...flowlinePromises]).then(() => {
-        // This block executes after all promises from populate_upstream and populate_flowlines have resolved
-        console.log('All updates are complete');
-    }).catch(error => {
-        console.error('An error occurred:', error);
-    });
-}
-
-function onMapClick(event) {
-    console.log(event);
-    // Extract the clicked coordinates
-    var lat = event.lngLat.lat;
-    var lng = event.lngLat.lng;
-
-    // Send an AJAX request to the Flask backend
-    fetch('/handle_map_interaction', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            coordinates: { lat: lat, lng: lng }
-        }),
-    })
-        .then(response => response.json())
-        .then(data => {
-
-            // if the cat_id is already in the dict, remove the key
-            if (data['cat_id'] in cat_id_dict) {
-                delete cat_id_dict[data['cat_id']];
-            }
-            else {
-                // temporary fix to only allow one basin to be selected
-                cat_id_dict = {};
-                // uncomment above line to allow multiple basins to be selected
-                cat_id_dict[data['cat_id']] = [lat, lng];
-            }
-            console.log('clicked on cat_id: ' + data['cat_id'] + ' coords :' + lat + ', ' + lng);
-
-
-            synchronizeUpdates();
-            //$('#selected-basins').text(Object.keys(cat_id_dict).join(', '));
-            // revert this line too
-            $('#selected-basins').text(Object.keys(cat_id_dict));
-
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-
-}
-
-function select_by_lat_lon() {
-    lat = $('#lat_input').val();
-    lon = $('#lon_input').val();
-    fetch('/handle_map_interaction', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            coordinates: { lat: lat, lng: lon }
-        }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            cat_id = data['cat_id'];
-            cat_id_dict[cat_id] = [lat, lon];
-            synchronizeUpdates();
-            $('#selected-basins').text(Object.keys(cat_id_dict));
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-$('#select-lat-lon-button').click(select_by_lat_lon);
-
-function select_by_id() {
-    cat_id_dict = {};
-    cat_ids = $('#cat_id_input').val();
-
-    cat_ids = cat_ids.split(',');
-    for (cat_id of cat_ids) {
-        cat_id_dict[cat_id] = [0, 0];
-    }
-    synchronizeUpdates();
-    $('#selected-basins').text(Object.keys(cat_id_dict));
-}
-
-$('#select-button').click(select_by_id);
-
-function clear_selection() {
-    cat_id_dict = {};
-    synchronizeUpdates();
-    $('#selected-basins').text(Object.keys(cat_id_dict));
-}
-
-$('#clear-button').click(clear_selection);
-
-function get_catid_from_gage_id(gage_id) {
-    return fetch('/get_catid_from_gage_id', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            gage_id: gage_id
-        }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            return data['cat_ids'];
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-function select_by_gage_id() {
-    gage_ids = $('#gage_id_input').val();
-    gage_ids = gage_ids.split(',');
-    for (gage_id of gage_ids) {
-        cat_ids = get_catid_from_gage_id(gage_id);
-        cat_ids.then(function (result) {
-            for (result of result) {
-                cat_id_dict[result] = [0, 0];
-            }
-            $('#selected-basins').text(Object.keys(cat_id_dict));
-        });
-    }
-    synchronizeUpdates();
-}
-
-$('#select-gage-button').click(select_by_gage_id);
-
-// // Initialize the map
-// var map = maplibregl.map('map', { crs: maplibregl.CRS.EPSG3857 }).setView([40, -96], 5);
-
-// //Create in-map Legend / Control Panel
-// var legend = maplibregl.control({ position: 'bottomright' });
-// // load in html template for the legend
-// legend.onAdd = function (map) {
-//     legend_div = maplibregl.DomUtil.create('div', 'custom_legend');
-//     return legend_div
-// };
-// legend.addTo(map);
-
 
 // add the PMTiles plugin to the maplibregl global.
 let protocol = new pmtiles.Protocol({metadata: true});
@@ -361,7 +33,6 @@ map.on('load', () => {
         'layout': {},
         'paint': {
             'line-width': 2,
-            // 'fill-color': colorDict.clearFill,
             'line-color': colorDict.flowlineToCatOutline,
         }
     });
@@ -379,16 +50,14 @@ map.on('load', () => {
     });
     map.addLayer({
         'id': 'catchments',
-        'type': 'line',
+        'type': 'fill',
         'source': 'conus',
         'source-layer': 'catchments',
         'layout': {},
         'paint': {
-            // 'fill-color': colorDict.clearFill,
-            // 'fill-outline-color': colorDict.nexusOutline,
-            'line-color': colorDict.nexusOutline,
-            'line-width': { "stops": [[7, 1], [13, 3]] },
-            'line-opacity': { "stops": [[7, 0], [11, 1]] }
+            'fill-color': colorDict.clearFill,
+            'fill-outline-color': colorDict.nexusOutline,
+            'fill-opacity': { "stops": [[7, 0], [11, 1]] }
         }
     });
     map.addLayer({
@@ -431,5 +100,97 @@ map.on('load', () => {
         "filter": ["any",["in", "divide_id", ""],]
     });
 });
-map.on('click', onMapClick);
+function update_map(cat_id, e) {
+    $('#selected-basins').text(cat_id)
+    map.setFilter('selected-catchments', ['any', ['in', 'divide_id', cat_id]]);
+    map.setFilter('upstream-catchments', ['any', ['in', 'divide_id', ""]])
+    
+    fetch('/get_upstream_catids', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(cat_id),
+    })
+    .then(response => response.json())
+    .then(data => {
+        map.setFilter('upstream-catchments', ['any', ['in', 'divide_id', ...data]]);
+        if (data.length === 0) { 
+            new maplibregl.Popup()
+            .setLngLat(e.lngLat)
+            .setHTML('No upstreams')
+            .addTo(map);
+        }
+    });
+}
+map.on('click', 'catchments', (e) => {
+    cat_id = e.features[0].properties.divide_id;    
+    update_map(cat_id, e);
+});
 
+
+// function clear_selection() {
+//     cat_id_dict = {};
+//     synchronizeUpdates();
+//     $('#selected-basins').text(Object.keys(cat_id_dict));
+// }
+
+// $('#clear-button').click(clear_selection);
+
+// function select_by_id() {
+//     cat_id = $('#cat_id_input').val();
+//     update_map(cat_id);
+//     // get the lat long for zoom
+//     fetch('/get_cat_xy', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(cat_id),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         map.flyTo({
+//             // These options control the ending camera position: centered at
+//             // the target, at zoom level 9, and north up.
+//             center: data,
+//             zoom: 9,
+//             bearing: 0,
+    
+//             // These options control the flight curve, making it move
+//             // slowly and zoom out almost completely before starting
+//             // to pan.
+//             speed: 0.2, // make the flying slow
+//             curve: 1, // change the speed at which it zooms out
+    
+//             // This can be any easing function: it takes a number between
+//             // 0 and 1 and returns another number between 0 and 1.
+//             easing(t) {
+//                 return t;
+//             },
+    
+//             // this animation is considered essential with respect to prefers-reduced-motion
+//             essential: true
+//         });
+//     });
+// }
+
+// $('#select-button').click(select_by_id);
+
+// function select_by_gage_id() {
+//     gage_ids = $('#gage_id_input').val();
+//     fetch('/get_upstream_catids', {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify(cat_id),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         map.setFilter('upstream-catchments', ['any', ['in', 'divide_id', ...data]]);
+//         if (data.length === 0) { 
+//             new maplibregl.Popup()
+//             .setLngLat(e.lngLat)
+//             .setHTML('No upstreams')
+//             .addTo(map);
+//         }
+//     });
+
+// }
+
+// $('#select-gage-button').click(select_by_gage_id);
