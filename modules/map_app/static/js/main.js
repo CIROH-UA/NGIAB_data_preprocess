2265abf52fc50b9cf7b79645a0ac213da537ea3d
Josh Cunningham <josh.cu@gmail.com>
2025-05-20 10:54:10 -0500

    set js indent to 2 spaces

--- modules/map_app/static/js/main.js
+++ modules/map_app/static/js/main.js
@@ -1,13 +1,13 @@
 var colorDict = {
-    selectedCatOutline: getComputedStyle(document.documentElement).getPropertyValue('--selected-cat-outline'),
-    selectedCatFill: getComputedStyle(document.documentElement).getPropertyValue('--selected-cat-fill'),
-    upstreamCatOutline: getComputedStyle(document.documentElement).getPropertyValue('--upstream-cat-outline'),
-    upstreamCatFill: getComputedStyle(document.documentElement).getPropertyValue('--upstream-cat-fill'),
-    flowlineToCatOutline: getComputedStyle(document.documentElement).getPropertyValue('--flowline-to-cat-outline'),
-    flowlineToNexusOutline: getComputedStyle(document.documentElement).getPropertyValue('--flowline-to-nexus-outline'),
-    nexusOutline: getComputedStyle(document.documentElement).getPropertyValue('--nexus-outline'),
-    nexusFill: getComputedStyle(document.documentElement).getPropertyValue('--nexus-fill'),
-    clearFill: getComputedStyle(document.documentElement).getPropertyValue('--clear-fill')
+  selectedCatOutline: getComputedStyle(document.documentElement).getPropertyValue('--selected-cat-outline'),
+  selectedCatFill: getComputedStyle(document.documentElement).getPropertyValue('--selected-cat-fill'),
+  upstreamCatOutline: getComputedStyle(document.documentElement).getPropertyValue('--upstream-cat-outline'),
+  upstreamCatFill: getComputedStyle(document.documentElement).getPropertyValue('--upstream-cat-fill'),
+  flowlineToCatOutline: getComputedStyle(document.documentElement).getPropertyValue('--flowline-to-cat-outline'),
+  flowlineToNexusOutline: getComputedStyle(document.documentElement).getPropertyValue('--flowline-to-nexus-outline'),
+  nexusOutline: getComputedStyle(document.documentElement).getPropertyValue('--nexus-outline'),
+  nexusFill: getComputedStyle(document.documentElement).getPropertyValue('--nexus-fill'),
+  clearFill: getComputedStyle(document.documentElement).getPropertyValue('--clear-fill')
 };
 
 // A function that creates a cli command from the interface
@@ -50,7 +50,7 @@ var style = 'https://communityhydrofabric.s3.us-east-1.amazonaws.com/map/styles/
 var colorScheme = "light";
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
   style = 'https://communityhydrofabric.s3.us-east-1.amazonaws.com/map/styles/dark-style.json';
-    colorScheme = "dark";
+  colorScheme = "dark";
 }
 var map = new maplibregl.Map({
   container: "map", // container id
@@ -130,54 +130,54 @@ map.on("load", () => {
 });
 
 function update_map(cat_id, e) {
-    $('#selected-basins').text(cat_id)
-    map.setFilter('selected-catchments', ['any', ['in', 'divide_id', cat_id]]);
-    map.setFilter('upstream-catchments', ['any', ['in', 'divide_id', ""]])
+  $('#selected-basins').text(cat_id)
+  map.setFilter('selected-catchments', ['any', ['in', 'divide_id', cat_id]]);
+  map.setFilter('upstream-catchments', ['any', ['in', 'divide_id', ""]])
 
-    fetch('/get_upstream_catids', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(cat_id),
-    })
-    .then(response => response.json())
-    .then(data => {
-        map.setFilter('upstream-catchments', ['any', ['in', 'divide_id', ...data]]);
-        if (data.length === 0) {
-            new maplibregl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML('No upstreams')
-            .addTo(map);
-        }
-    });
+  fetch('/get_upstream_catids', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(cat_id),
+  })
+  .then(response => response.json())
+  .then(data => {
+    map.setFilter('upstream-catchments', ['any', ['in', 'divide_id', ...data]]);
+    if (data.length === 0) {
+      new maplibregl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML('No upstreams')
+        .addTo(map);
+    }
+  });
 }
 map.on('click', 'catchments', (e) => {
-    cat_id = e.features[0].properties.divide_id;
-    update_map(cat_id, e);
+  cat_id = e.features[0].properties.divide_id;
+  update_map(cat_id, e);
 });
 
 // Create a popup, but don't add it to the map yet.
 const popup = new maplibregl.Popup({
-    closeButton: false,
-    closeOnClick: false
+  closeButton: false,
+  closeOnClick: false
 });
 
 map.on('mouseenter', 'conus_gages', (e) => {
-    // Change the cursor style as a UI indicator.
-    map.getCanvas().style.cursor = 'pointer';
+  // Change the cursor style as a UI indicator.
+  map.getCanvas().style.cursor = 'pointer';
 
-    const coordinates = e.features[0].geometry.coordinates.slice();
-    const description = e.features[0].properties.hl_uri + "<br> click for more info";
+  const coordinates = e.features[0].geometry.coordinates.slice();
+  const description = e.features[0].properties.hl_uri + "<br> click for more info";
 
-    // Ensure that if the map is zoomed out such that multiple
-    // copies of the feature are visible, the popup appears
-    // over the copy being pointed to.
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    }
+  // Ensure that if the map is zoomed out such that multiple
+  // copies of the feature are visible, the popup appears
+  // over the copy being pointed to.
+  while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+  }
 
-    // Populate the popup and set its coordinates
-    // based on the feature found.
-    popup.setLngLat(coordinates).setHTML(description).addTo(map);
+  // Populate the popup and set its coordinates
+  // based on the feature found.
+  popup.setLngLat(coordinates).setHTML(description).addTo(map);
 });
 
 map.on("mouseleave", "conus_gages", () => {
@@ -189,7 +189,7 @@ map.on("click", "conus_gages", (e) => {
   //  https://waterdata.usgs.gov/monitoring-location/02465000
   window.open(
     "https://waterdata.usgs.gov/monitoring-location/" +
-      e.features[0].properties.hl_link,
+    e.features[0].properties.hl_link,
     "_blank",
   );
 });
