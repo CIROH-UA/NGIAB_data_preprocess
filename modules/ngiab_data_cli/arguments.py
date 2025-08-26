ab9493c22b98df8aa09c1b8d95a76cc8a9516a1d
Quinn Lee <qylee@ua.edu>
2025-08-26 13:06:38 -0500

    added CLI option to choose output root folder

--- modules/ngiab_data_cli/arguments.py
+++ modules/ngiab_data_cli/arguments.py
@@ -11,7 +11,7 @@ def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Subsetting hydrofabrics, forcing generation, and realization creation"
     )
-    group = parser.add_mutually_exclusive_group(required=True)
+    group = parser.add_mutually_exclusive_group(required=False)
 
     parser.add_argument(
         "--change_output_dir",
